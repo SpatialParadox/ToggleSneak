766c491294d3cdcfb949b5215dc6a62eb3e6fea4
SpatialParadox <huntzorz@gmail.com>
2020-06-20 17:28:11 +0100

    Add name parameter to @Mod

--- src/main/java/com/condolence/togglesneak/ToggleSneakMod.java
+++ src/main/java/com/condolence/togglesneak/ToggleSneakMod.java
@@ -12,10 +12,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 import java.io.File;
 
-@Mod(modid = ToggleSneakMod.MOD_ID, version = ToggleSneakMod.VERSION)
+@Mod(modid = ToggleSneakMod.MOD_ID, version = ToggleSneakMod.VERSION, name = ToggleSneakMod.NAME)
 public class ToggleSneakMod {
     public static final String MOD_ID = "togglesneak";
     public static final String VERSION = "1.0";
+    public static final String NAME = "Toggle Sneak";
 
     private Minecraft minecraft;
     private ToggleSneakSettings settings;
